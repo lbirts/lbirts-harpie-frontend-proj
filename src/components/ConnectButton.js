fc776f075d9aafc4b494039a675a972f040d96f5
lbirts <birtslauren@gmail.com>
2022-09-07 21:26:50 -0500

    add connect function

--- src/components/ConnectButton.js
+++ src/components/ConnectButton.js
@@ -1,8 +1,17 @@
 import React from 'react'
+import { ethers } from "ethers";
 
 export default function ConnectButton() {
+
+    const connectToMetamask = async() => {
+        const provider = new ethers.providers.Web3Provider(window.ethereum)
+        await provider.send("eth_requestAccounts", [])
+        const signer = provider.getSigner()
+        console.log("Account:", await signer.getAddress())
+    }
+
 	return (
-		<button>
+		<button onClick={connectToMetamask}>
 			Connect Wallet
 		</button>
 	)
